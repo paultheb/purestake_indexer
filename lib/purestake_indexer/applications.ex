94ad159e51cdcff582e41aa838d6676a33b287e3
Paul Theberge <theberge.paul@gmail.com>
2022-07-09 11:16:31 -0400

    Updated applications.ex

--- lib/purestake_indexer/applications.ex
+++ lib/purestake_indexer/applications.ex
@@ -1,8 +1,11 @@
 defmodule PureStakeIndexer.Applications do
-
   import PureStakeIndexer
 
-  @spec get([HTTPoison.Base.params]) :: any # update this return type to be the right struct
+  @moduledoc """
+  Handles request to get all applications based on supplied optional parameters.
+  """
+
+  @spec get([HTTPoison.Base.params()]) :: PureStakeIndexer.response()
   def get(params \\ []) do
     options = [params: params]
     get("/v2/applications", [], options)
